1805e39fc7bfb94f10ead6ee73cf630aa1e50510
Jeppe Traberg <J.TrabergS@gmail.com>
2017-03-08 13:51:18 +0100

    bette start

---
+++ DataAccessTier/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessTier
+{
+    public class Person
+    {
+        private string Fornavn { get; set; }
+        private string Mellemnavn { get; set; }
+        private string Efternavn { get; set; }
+        private string Persontype { get; set; }
+    }
+}
---
+++ DataAccessTier/PersonDataUtil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessTier
+{
+    public class PersonDataUtil
+    {
+        private Person locPerson;
+        private SqlConnection conn;
+
+        public PersonDataUtil()
+        {
+            conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Handinv2;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True");
+        }
+
+        public void insertPerson(Person person)
+        {
+            try
+            {
+                conn.Open();
+
+                string insertString = @"
+                    INSERT INTO Person"
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+    }
+}
+
+
+// Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Handinv2;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True
